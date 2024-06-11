6b827d0615fc9593c539460894fa94ec438a6c30
KIMCHOONGMAN <cm730712@gmail.com>
2024-06-11 21:56:30 +0900

    각 컴포넌트의 state 및 props 타입스크립트로 수정

--- src/App.ts
+++ src/App.ts
@@ -1,17 +1,13 @@
-import { Component } from './core/heropy'
-import TheHeader from './components/TheHeader'
-import TheFooter from './components/TheFooter'
+import { Component } from "./core/heropy";
+import TheHeader from "./components/TheHeader.ts";
+import TheFooter from "./components/TheFooter";
 
 export default class App extends Component {
   render() {
-    const theHeader = new TheHeader().el
-    const theFooter = new TheFooter().el
-    const routerView = document.createElement('router-view')
+    const theHeader = new TheHeader().el;
+    const theFooter = new TheFooter().el;
+    const routerView = document.createElement("router-view");
 
-    this.el.append(
-      theHeader,
-      routerView,
-      theFooter
-    )
+    this.el.append(theHeader, routerView, theFooter);
   }
 }
